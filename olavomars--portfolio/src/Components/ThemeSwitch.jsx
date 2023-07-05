43afbabb118717c4116cbd411c1a3ca3ecf5857c
Olavo Mars <olavoamneves@gmail.com>
2023-07-05 12:07:01 -0400

    adding themeSwitch component

--- olavomars--portfolio/src/Components/ThemeSwitch.jsx
+++ olavomars--portfolio/src/Components/ThemeSwitch.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { ThemeContext } from '../Contexts/theme-context';
 import { useContext } from 'react';
+
+import '@theme-toggles/react/css/Within.css';
+import { Within } from '@theme-toggles/react';
 import '../styles/theme-variables.scss';
 
 export const ThemeSwitch = () => {
@@ -12,25 +15,12 @@ export const ThemeSwitch = () => {
   };
 
   return (
-    <div>
-      <div className='header-content'>
-        <div className='toggle-btn-section'>
-          <div className={`toggle-checkbox m-vertical-auto`}>
-            <input
-              className='toggle-btn__input'
-              type='checkbox'
-              name='checkbox'
-              onChange={handleThemeChange}
-              checked={theme === 'light'}
-            />
-            <button
-              type='button'
-              className={`toggle-btn__input-label`}
-              onClick={handleThemeChange}
-            ></button>
-          </div>
-        </div>
-      </div>
+    <div className='themeSwitch__btn-container'>
+      <Within
+        duration={750}
+        className='themeSwitch__btn'
+        onToggle={handleThemeChange}
+      />
     </div>
   );
 };
